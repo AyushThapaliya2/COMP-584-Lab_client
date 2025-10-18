421e6c09811b7e18740e839b1a9369e875e2f81b
Jacob-Parcell <jakeparcell14@gmail.com>
2025-10-17 18:16:01 -0700

    Oct 17th Commit

---
+++ src/app/country-population/country-population.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { PopulationData } from './population-data';
+import { environment } from '../../environments/environment.development';
+
+@Component({
+  selector: 'app-country-population',
+  imports: [],
+  templateUrl: './country-population.html',
+  styleUrl: './country-population.scss'
+})
+export class CountryPopulation {
+  country!: PopulationData;
+
+  constructor(http: HttpClient) {
+    http.get<PopulationData>(environment.apiUrl + 'api/Countries/population/26').subscribe(result => {
+      this.country = result;
+    });
+  }
+}
